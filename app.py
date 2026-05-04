15e9d99e65f8712c2017a250e3c7a4821609a738
Carlos Eduardo Pereira Dutra <eduardodutra@outlook.com.br>
2026-05-04 11:00:17 -0300

    Fix: replace unsafe yaml.load with safe_load

--- app.py
+++ app.py
@@ -2,4 +2,5 @@
 import yaml
 
 def load_config(user_input):
-    return yaml.load(user_input, Loader=yaml.Loader)
+#    return yaml.load(user_input, Loader=yaml.Loader)
+    return yaml.safe_load(user_input)
